bb471a1a829f2564cf5cf096b3f279411e43a25f
maj39623 <maj39623@gmail.com>
2022-05-15 19:29:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# soleran
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/soleran)
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent} from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppComponent } from './app.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    AppRoutingModule, 
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ) 
+  ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent],
+  bootstrap:    [ AppComponent ],
+  providers: []
+})
+export class AppModule { }
